5fe19fe8e0a7a355f3c0517b16b0e46a99905906
StefS99 <stefan.simic9977@gmail.com>
2022-09-21 04:43:36 +0200

    model liste

--- poslovi/app/Models/Listing.php
+++ poslovi/app/Models/Listing.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Listing extends Model
 {
     use HasFactory;
+
+    //Ovo moramo dodati, jer nam baza ne bi dozvolila //Ali moze da izmenimo tako što stavimo unguard() u AppServiceProvider Model::unguard();
+  //  protected $fillable = ['title', 'company', 'location', 'website', 'email', 'description', 'tags'];
+
+    public function scopeFilter($query, array $filters){
+       // dd($filters['tag']);
+       if($filters['tag'] ?? false){  //?? Ako nije netačno, onda idi dalje
+            $query -> where('tags','like','%'.request('tag').'%'); //% sve što dolazi posle i pre ?tags=laravel (tag) npr
+       }
+
+       if($filters['search'] ?? false){  //?? Ako nije netačno, onda idi dalje
+        $query->where('title','like','%'.request('search').'%')
+               ->orWhere('description','like','%'.request('search').'%')
+               ->orWhere('tags','like','%'.request('search').'%');
+        }
+    }
 }
